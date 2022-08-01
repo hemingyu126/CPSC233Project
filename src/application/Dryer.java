bb8bed810484c2bd20b3b6756831a794279820f3
hemingyu <hemingyu@localhost>
2022-08-01 17:31:36 -0600

    Added a new class Dryer.

---
+++ src/application/Dryer.java
@@ -0,0 +1,23 @@
+package application;
+
+public class Dryer extends Machine {
+
+	@Override
+	public String start(String id, String mode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String pause() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String pause(String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

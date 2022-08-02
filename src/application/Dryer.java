16f83a12e9a4aa0e01503682259030a55ec2a3cd
hemingyu <hemingyu@localhost>
2022-08-01 18:51:49 -0600

    Implemented the Dryer class and improved encapsulation.

--- src/application/Dryer.java
+++ src/application/Dryer.java
@@ -1,23 +1,45 @@
 package application;
 
 public class Dryer extends Machine {
-
+	
+	private String temperature;
+	
+	Dryer(String machineName, double price){
+		this.setName(machineName);
+		this.setPricePerUse(price);
+	}
+	
+	public void setTemperature(String tempToSet) {
+		temperature = tempToSet;
+	}
+	
+	public String getTemperature() {
+		return temperature;
+	}
+	
 	@Override
-	public String start(String id, String mode) {
-		// TODO Auto-generated method stub
-		return null;
+	public String start(String id, String tempToSet) {
+		if (!this.getStatus())
+			return "Sorry, this machine is not available to use.";
+		else if (this.getInUse())
+			return "This machine is currently in use. Please find another one.";
+		else {
+			this.setTemperature(tempToSet);;
+			this.setInUse(true);
+			return "";
+		}
 	}
 
 	@Override
 	public String pause() {
-		// TODO Auto-generated method stub
-		return null;
+		this.setInUse(false);
+		temperature = "";
+		return "";
 	}
 
 	@Override
 	public String pause(String password) {
-		// TODO Auto-generated method stub
-		return null;
+		return pause();
 	}
 
 }

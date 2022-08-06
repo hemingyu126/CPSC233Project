caa0b92c5a5944507ee974d714310b9d6bca7114
hemingyu <hemingyu@localhost>
2022-08-06 02:55:14 -0600

    Implemented the deposit method

--- src/application/DataCenter.java
+++ src/application/DataCenter.java
@@ -9,6 +9,18 @@ public class DataCenter {
 	DataCenter(){
 		customers = new ArrayList<Customer>();
 		machines = new ArrayList<Machine>();
+		WashingMachine machine1 = new WashingMachine("Washing Machine 1", 1.75, "wm1");
+		WashingMachine machine2 = new WashingMachine("Washing Machine 2", 1.75, "wm2");
+		WashingMachine machine3 = new WashingMachine("Washing Machine 3", 1.75, "wm3");
+		Dryer dryer1 = new Dryer("Dryer 1", 1.25);
+		Dryer dryer2 = new Dryer("Dryer 2", 1.25);
+		Dryer dryer3 = new Dryer("Dryer 3", 1.25);
+		machines.add(machine1);
+		machines.add(machine2);
+		machines.add(machine3);
+		machines.add(dryer1);
+		machines.add(dryer2);
+		machines.add(dryer3);
 	}
 	
 	public Customer getCustomer(String customerID){
@@ -27,19 +39,14 @@ public class DataCenter {
 		return null;
 	}
 	
-	public boolean addCustomer(Customer newCustomter) {
-		boolean toAdd = true;
-		
-		for (Customer c: customers)
-			if (c.getCustomerID().equals(newCustomter.getCustomerID())) {
-
-				toAdd = false;
-				break;
-			}
-		if (toAdd)
-			customers.add(newCustomter);
-		return toAdd;
+	public String addCustomer(String requestedID) {
+		for (char c: requestedID.toCharArray())
+			if (!Character.isDigit(c))
+				return "Customer ID should an integer and cannot contain:" + c;
+		if (requestedID.length()!=4)
+			return "Customer ID must be 4 digits. The ID you requested is "+requestedID.length()+ " digits";
+		Customer toAdd = new Customer(requestedID);
+		customers.add(toAdd);
+		return "";
 	}
-
-
 }
